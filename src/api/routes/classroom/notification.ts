fbbe85961f8ebe629ea526da95b097ba22082ae1
Ph4t3 <mail@nikhilrajesh.com>
2020-12-20 04:07:59 +0530

    Added /classroom/notificatin/add endpoint

--- src/api/routes/classroom/notification.ts
+++ src/api/routes/classroom/notification.ts
@@ -1,6 +1,35 @@
-import { Router } from 'express';
+import { Router, Response, NextFunction } from 'express';
+import { celebrate, Joi } from 'celebrate';
+import logger from '../../../loaders/logger';
+import middlewares from '../../middlewares';
+import { IAuth } from '../../../interfaces/Middleware';
+import NotificationService from '../../../services/classroom/notification';
 const route = Router();
 
 export default (app: Router) => {
-  app.use('/classroom/notifications', route);
+  app.use('/classroom/notification', route);
+
+  route.post(
+    '/add',
+    middlewares.isAuth,
+    middlewares.requiredRole('CLASSROOM_ADMIN'),
+    celebrate({
+      body: Joi.object({
+        classroom_code: Joi.string().required(),
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+      }),
+    }),
+    async (req: IAuth, res: Response, next: NextFunction) => {
+      logger.debug('Calling Notification add endpoint with body: %o', req.body);
+      try {
+        const notificationServiceInstance = new NotificationService();
+        const result = await notificationServiceInstance.AddNotification(req.body, req.token);
+        return res.json(result).status(200);
+      } catch (e) {
+        logger.error('🔥 error: %o', e);
+        return next(e);
+      }
+    },
+  );
 };
---
+++ src/services/classroom/notification.ts
@@ -0,0 +1,71 @@
+import logger from '../../loaders/logger';
+import db from '../../loaders/db';
+import ShortUniqueId from 'short-unique-id';
+import { IDefaultResponse } from '../../interfaces/Response';
+import { IGetUserInfo } from '../../interfaces/Users';
+import { IAddNotification } from '../../interfaces/Notification';
+
+export default class NotificationService {
+  public async AddNotification(
+    notif: IAddNotification,
+    user: IGetUserInfo,
+  ): Promise<IDefaultResponse> {
+    let conn = null;
+    try {
+      conn = await db.getConnection();
+      logger.silly('Transaction Begin');
+      await conn.beginTransaction();
+
+      // Find classroom
+      logger.silly('Fetching the classroom info');
+      const [
+        classInfo,
+      ] = await conn.query('SELECT * FROM Classroom WHERE Classroom.classroom_code = ?', [
+        notif.classroom_code,
+      ]);
+      if (classInfo.length === 0) {
+        throw new Error('Invalid Classroom');
+      }
+
+      // Check if Admin has Perms
+      logger.silly('Checking classroom admin perms');
+      if (user.user_type !== 'SUPER_ADMIN' && classInfo[0].admin_name !== user.username) {
+        throw new Error('Not enough Permissions');
+      }
+
+      // Find a valid classroom_code
+      logger.silly('Generating UUID');
+      const uid = new ShortUniqueId();
+      var UUID: string;
+      while (1) {
+        UUID = uid();
+        const collidingClasses = await conn.query(
+          'SELECT * FROM Notification WHERE Notification.notification_id = ?',
+          [UUID],
+        );
+        if (collidingClasses[0].length === 0) {
+          break;
+        }
+      }
+
+      // Insert db record
+      logger.silly('Creating notification db record');
+      const results = await conn.query('INSERT INTO Notification VALUES (?, ?, ?, DEFAULT, ?)', [
+        UUID,
+        notif.title,
+        notif.description,
+        notif.classroom_code,
+      ]);
+
+      await conn.commit();
+      logger.silly('Transaction Commited');
+      return { success: true, message: 'Notification added' };
+    } catch (error) {
+      logger.error(error);
+      if (conn) await conn.rollback();
+      throw error;
+    } finally {
+      if (conn) await conn.release();
+    }
+  }
+}
